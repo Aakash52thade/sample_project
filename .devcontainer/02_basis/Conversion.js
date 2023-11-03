5959982c6c23191cf9b489b39a687387ba11a365
Aakash52thade <117812701+Aakash52thade@users.noreply.github.com>
2023-11-03 17:01:31 +0000

    Conversion of Datatype

---
+++ .devcontainer/02_basis/Conversion.js
@@ -0,0 +1,25 @@
+let score = 33;
+console.log(typeof score);
+
+let ak  = "45";
+let valueofAK = Number(ak); // here "45" is String and now change in Number;
+console.log(typeof valueofAK);
+console.log(valueofAK);
+
+// "33" => 33;
+// "33abc" => NaN == Not a Number
+// true => 1; false => 0
+
+//Now we are check in Boolean;
+let isLoggedIn = 1;
+
+let booleanis = Boolean(isLoggedIn);
+console.log(booleanis);
+
+
+//Now we are check in Boolean;
+
+let some = 18;
+let num = String(some);
+console.log(num);
+console.log(typeof num);
